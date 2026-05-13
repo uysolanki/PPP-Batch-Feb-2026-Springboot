57665442c6874bd579d45238c8c6deb8746e507d
uysolanki <uysolanki@gmail.com>
2026-05-13 09:50:01 +0530

    3 ways to accept input

--- src/main/java/com/itp/amazon/controller/StudentController.java
+++ src/main/java/com/itp/amazon/controller/StudentController.java
@@ -2,11 +2,16 @@ package com.itp.amazon.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.itp.amazon.entity.Student;
 import com.itp.amazon.service.StudentService;
+
+import jakarta.websocket.server.PathParam;
 
 @RestController	//return json
 //@Controller     //return html
@@ -29,6 +34,87 @@ public class StudentController {
 		
 	}
 	
+	@RequestMapping("/saveStudentUsingRequestParam")
+	public Student saveStudentUsingRequestParam(
+			@RequestParam("a") String studentName,
+			@RequestParam("b") String deptName,
+			@RequestParam("c") double studentPer
+			)
+	{
+		Student s1=Student.builder()
+				.sname(studentName)
+				.dname(deptName)
+				.per(studentPer)
+				.build();
+		
+		return studentService.saveStudent(s1);
+		//return "Record Saved";
+		
+	}
+	
+	@RequestMapping("/saveStudentUsingRequestParam1")
+	public Student saveStudentUsingRequestParam1(
+			@RequestParam String studentName,
+			@RequestParam String deptName,
+			@RequestParam double studentPer
+			)
+	{
+		Student s1=Student.builder()
+				.sname(studentName)
+				.dname(deptName)
+				.per(studentPer)
+				.build();
+		
+		return studentService.saveStudent(s1);
+		//return "Record Saved";
+		
+	}
+	
+	
+	@RequestMapping("/saveStudentUsingPathVariable/{a}/{b}/{c}")
+	public Student saveStudentUsingPathVariable(
+			@PathVariable("a") String studentName,
+			@PathVariable("b") String deptName,
+			@PathVariable("c") double studentPer
+			)
+	{
+		Student s1=Student.builder()
+				.sname(studentName)
+				.dname(deptName)
+				.per(studentPer)
+				.build();
+		
+		return studentService.saveStudent(s1);
+		//return "Record Saved";
+		
+	}
+	
+	@RequestMapping("/saveStudentUsingPathVariable1/{studentName}/{deptName}/{studentPer}")
+	public Student saveStudentUsingPathVariable1(
+			@PathVariable String studentName,
+			@PathVariable String deptName,
+			@PathVariable double studentPer
+			)
+	{
+		Student s1=Student.builder()
+				.sname(studentName)
+				.dname(deptName)
+				.per(studentPer)
+				.build();
+		
+		return studentService.saveStudent(s1);
+		//return "Record Saved";
+		
+	}
+	
+	@RequestMapping("/saveStudentUsingRequestBody")
+	public Student saveStudentUsingRequestBody(@RequestBody Student s1)
+	{
+		return studentService.saveStudent(s1);
+		
+	}
+	
+	
 	
 	@RequestMapping("/getStudent")
 	public Student getStudent()
@@ -40,3 +126,12 @@ public class StudentController {
 	}
 
 }
+
+/*
+{
+    "rno": 8,
+    "sname": "Vedant",
+    "dname": "Science",
+    "per": 78.6
+}
+*/
